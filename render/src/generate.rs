4e121ec486fe6963c176dee52e4a68b0d81ebebd
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-12-03 19:44:21 +0700

    format raw html strings

--- render/src/generate.rs
+++ render/src/generate.rs
@@ -3,69 +3,94 @@ use card::component::frame::Direction::*;
 use card::component::text::Font::*;
 use card::component::Component;
 
-// generate.rs
-pub fn gen(component: &Component, global_theme: &Shade) -> String {
+/// Generate raw html string
+pub fn gen_html(component: &Component, global_theme: &Shade) -> String {
     let theme = match global_theme {
         Shade::Dark(theme) | Shade::Light(theme) => theme,
     };
     match component {
         Component::Text(text) => match text.font {
             Label => format!(
-                "<p class=\"text-lg font-semibold\" style=\"color: {}\">{}</p>",
-                theme.primary,
-                text.text
+                "<p class=\"text-lg font-semibold\" style=\"color: {}\">
+                    {}
+                </p>",
+                theme.primary, text.text
             ),
             SubLabel => format!(
-                "<p class=\"text-md font-medium\" style=\"color: {}\">{}</p>",
-                theme.secondary,
-                text.text
+                "<p class=\"text-md font-medium\" style=\"color: {}\">
+                    {}
+                </p>",
+                theme.secondary, text.text
             ),
             Text => format!(
-                "<p class=\"text-sm\" style=\"color: {}\">{}</p>",
-                theme.thirdly,
-                text.text
+                "<p class=\"text-sm\" style=\"color: {}\">
+                    {}
+                </p>",
+                theme.thirdly, text.text
             ),
             Minor => format!(
-                "<p class=\"text-xs text-gray-500\">{}</p>",
+                "<p class=\"text-xs text-gray-500\">
+                    {}
+                </p>",
                 text.text
             ),
         },
         Component::Link(link) => format!(
-            "<a href=\"{}\" class=\"text-blue-600 hover:text-blue-800\"><div>{}{}</div></a>",
-            link.href, link.text.and_then(|x| Some(gen(&x, &global_theme))).unwrap_or_default(), link.icon.and_then(|x| Some(gen(&x, &global_theme))).unwrap_or_default()
+            "<a href=\"{}\" class=\"text-blue-600 hover:text-blue-800\">
+                <div>
+                    {}{}
+                </div>
+            </a>",
+            link.href,
+            link.text
+                .and_then(|x| Some(gen_html(&x, &global_theme)))
+                .unwrap_or_default(),
+            link.icon
+                .and_then(|x| Some(gen_html(&x, &global_theme)))
+                .unwrap_or_default()
         ),
         Component::Field(field) => format!(
             "<input type=\"text\" value=\"{}\" style=\"border-color: {}; padding: {}px;\" />",
-            gen(field.title, global_theme) ,
+            gen_html(field.title, global_theme),
             theme.border,
             theme.border_pudding
         ),
         Component::Icon(icon) => format!(
-            "<i class=\"\" style=\"color: {};\">{}</i>",
+            "<i class=\"\" style=\"color: {};\">
+                {}
+            </i>",
             theme.primary,
             icon.as_str(),
         ),
         Component::Frame(frame) => match frame.direction {
             Vertical => format!(
-                "<div class=\"flex flex-col\" style=\"border-color: {}; padding: {}px;\">{}</div>",
+                "<div class=\"flex flex-col\" style=\"border-color: {}; padding: {}px;\">
+                    {}
+                </div>",
                 theme.border,
                 theme.frame_pudding,
                 frame_gen(frame.data, global_theme)
             ),
             Horizontal => format!(
-                "<div class=\"flex\" style=\"border-color: {}; padding: {}px;\">{}</div>",
+                "<div class=\"flex\" style=\"border-color: {}; padding: {}px;\">
+                    {}
+                </div>",
                 theme.border,
                 theme.frame_pudding,
                 frame_gen(frame.data, global_theme)
             ),
             ReversVertical => format!(
-                "<div class=\"flex flex-col-reverse\" style=\"border-color: {}; padding: {}px;\">{}</div>",
+                "<div class=\"flex flex-col-reverse\" style=\"border-color: {}; padding: {}px;\">
+                    {}
+                </div>",
                 theme.border,
                 theme.frame_pudding,
                 frame_gen(frame.data, global_theme)
             ),
             ReversHorizontal => format!(
-                "<div class=\"flex flex-row-reverse\" style=\"border-color: {}; padding: {}px;\">{}</div>",
+                "<div class=\"flex flex-row-reverse\" style=\"border-color: {}; padding: {}px;\">
+                    {}
+                </div>",
                 theme.border,
                 theme.frame_pudding,
                 frame_gen(frame.data, global_theme)
@@ -75,5 +100,10 @@ pub fn gen(component: &Component, global_theme: &Shade) -> String {
 }
 
 fn frame_gen(frame: &[Component<'_>], theme: &Shade) -> String {
-    frame.iter().map(|x| gen(x, &theme)).collect::<String>()
+    frame
+        .iter()
+        .map(|x| gen_html(x, &theme))
+        .collect::<String>()
 }
+
+pub fn gen_leptos_component() {}
