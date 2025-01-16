df28020d67ed4dd50f85a2a71a7489cd832c1d07
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2025-01-16 21:59:27 +0700

    remove unessary html generating

--- sss-cli/src/commands/gen.rs
+++ sss-cli/src/commands/gen.rs
@@ -1,10 +1,7 @@
 use tokio::fs;
 use tracing::{info, instrument};
 
-use crate::{
-    cli::args::Args,
-    tools::{layout_build, load},
-};
+use crate::{cli::args::Args, tools::load, HTML};
 
 #[instrument(skip(args))]
 pub async fn command_gen(args: &Args) -> anyhow::Result<()> {
@@ -15,12 +12,9 @@ pub async fn command_gen(args: &Args) -> anyhow::Result<()> {
         .map_err(|e| anyhow::anyhow!("Load failed: {}", e))?;
     info!("Load successfully!");
     info!("Try to generate html");
-    let html = layout_build(&args.layout)
-        .await
-        .map_err(|e| anyhow::anyhow!("Layout build error: {}", e))?;
     info!("Html generated successfully!");
     info!("Try to write html on disk by path: {}", &args.file_output);
-    fs::write(&args.file_output, &html).await?;
+    fs::write(&args.file_output, &*HTML.read().await).await?;
     info!("Html successfully saved by path: {}", &args.file_output);
     Ok(())
 }
