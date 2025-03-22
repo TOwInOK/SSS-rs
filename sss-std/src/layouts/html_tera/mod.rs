7d1a43e88a67bba1a459490aead8e83ab30afc51
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2025-03-22 17:40:29 +0700

    fix naming of layout macros

--- sss-std/src/layouts/html_tera/mod.rs
+++ sss-std/src/layouts/html_tera/mod.rs
@@ -1,7 +1,7 @@
 pub mod html_tera_builder;
 
+use html_layouts_derive::generate_layouts;
 pub use html_tera_builder::{HtmlTeraFinalize, HtmlTeraRender};
-use layouts_derive::generate_layouts;
 use serde::{Deserialize, Serialize};
 use sss_core::Settings;
 use theme::Theme;
